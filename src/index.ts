aa0119749a03ffd35df245254605f24c49a1b5a0
Mitchell Merry <mitchellinminecraft@gmail.com>
2022-01-15 19:27:11 +1100

    Function-ised

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import readline from 'readline';
 import JSONStream from 'JSONStream';
 import prompt from "prompt-sync";
 const promptSync = prompt({ sigint: true });
-// const prompt = require("prompt-sync")();
-
-const data = "AUS_SUB.geojson";
-const filter = "LVP_SUB";
-const filterProp = "SSC_NAME_2016";
-const changeValueToFilterValue = true;
 
 /**
  * @param filterPath ./data/{filterPath}.txt
@@ -38,63 +32,65 @@ function getFilterValues(filterPath: string): Promise<string[]> {
     });
 }
 
-/* Read and filter */
-let filterVals = await getFilterValues(filter);
-const valueCount = filterVals.length;
+function filterDataset(dataFilepath, filterFilepath, filterProperty, changeValueToFilterValue = true): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+        /* Read and filter */
+        let filterVals = await getFilterValues(filterFilepath);
+        const valueCount = filterVals.length;
 
-const dataFilepath = `./data/${data}`;
-const stream = fs.createReadStream(dataFilepath).pipe(JSONStream.parse('features.*'));
+        const stream = fs.createReadStream(`./data/${dataFilepath}`).pipe(JSONStream.parse('features.*'));
+        const filteredData: FeatureCollection = { type: "FeatureCollection", features: [] };
 
-const newGJ: FeatureCollection = {
-    type: "FeatureCollection",
-    features: []
-};
+        stream.on('data', (data: Feature) => {
+            const val = data.properties[filterProperty];
+            const matches = filterVals.filter(filterVal => val.includes(filterVal));
+            let match = undefined;
 
-stream.on('data', (data: Feature) => {
-    const val = data.properties[filterProp];
-    const matches = filterVals.filter(filterVal => val.includes(filterVal));
-    let match = undefined;
+            // No matches found
+            if(matches.length === 0) return;
 
-    // No matches found
-    if(matches.length === 0) return;
+            if(matches.length === 1 && matches[0] === val) match = matches[0];
+            // If multiple possible matches, or a single non-exact match, ask:
+            else {
+                console.log(`\nFound possible matches for "${val}":`);
+                console.log(matches.map((match, matchIndex) => `${matchIndex+1}) ${match}`).join('\n'));
+                const res = parseInt(promptSync(`Choose option [none]: `));
+                
+                // Invalid option, so assume we use none.
+                if(isNaN(res) || res < 1 || res > matches.length) {
+                    console.log("Using none...\n");
+                    return;
+                }
 
-    if(matches.length === 1 && matches[0] === val) match = matches[0];
-    // If multiple possible matches, or a single non-exact match, ask:
-    else {
-        console.log(`\nFound possible matches for "${val}":`);
-        console.log(matches.map((match, matchIndex) => `${matchIndex+1}) ${match}`).join('\n'));
-        const res = parseInt(promptSync(`Choose option [none]: `));
-        
-        // Invalid option, so assume we use none.
-        if(isNaN(res) || res < 1 || res > matches.length) {
-            console.log("Using none...\n");
-            return;
-        }
+                match = matches[res-1];
+            }
+            
+            // Renames "Greendale (Liverpool - NSW)" to "Greendale" for example, which is what we usually want.
+            if(changeValueToFilterValue) data.properties[filterProperty] = match;
 
-        match = matches[res-1];
-    }
-    
-    // Renames "Greendale (Liverpool - NSW)" to "Greendale" for example, which is what we usually want.
-    if(changeValueToFilterValue) data.properties[filterProp] = match;
+            console.log(`[${valueCount-filterVals.length+1}/${valueCount}] Found ${data.properties[filterProperty]}...`);
+            filteredData.features.push(data);
 
-    console.log(`[${valueCount-filterVals.length+1}/${valueCount}] Found ${data.properties[filterProp]}...`);
-    newGJ.features.push(data);
+            filterVals = filterVals.filter(i => i !== match);
+        });
 
-    filterVals = filterVals.filter(i => i !== match);
-});
+        stream.on('close', () => {
+            if(filterVals.length !== 0) {
+                /* Output filtered values */
+                console.log(`Found ${filteredData.features.length}:`);
+                console.log(filteredData.features.map(feature => feature.properties[filterProperty]));
 
-stream.on('close', () => {
-    if(filterVals.length !== 0) {
-        /* Output filtered values */
-        console.log(`Found ${newGJ.features.length}:`);
-        console.log(newGJ.features.map(feature => feature.properties[filterProp]));
+                console.log(`Missing: ${filterVals.length}:`);
+                console.log(filterVals);
+            } else {
+                console.log("Found all filter values!");
+            }
+            
+            /* Save */
+            fs.writeFileSync(`./data/${filterFilepath}.geojson`, JSON.stringify(filteredData));
+            resolve();
+        });
+    })
+}
 
-        console.log(`Missing: ${filterVals.length}:`);
-        console.log(filterVals);
-    } else {
-        console.log("Found all filter values!");
-    }
-    
-    /* Save */
-    fs.writeFileSync(`./data/${filter}.geojson`, JSON.stringify(newGJ));
-});
+filterDataset("AUS_SUB.geojson", "LVP_SUB", "SSC_NAME_2016");
